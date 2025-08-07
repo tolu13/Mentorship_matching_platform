a670470b93902fe938aadaa29b5e2819f3658b1b
tolu13 <tolulopeolanrewaju8@fmail.com>
2025-08-07 13:48:14 +0100

    added a package

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import  React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </BrowserRouter>
 );
